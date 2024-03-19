b8c4d4b4b8b306546b855de85707c281d927c390
Arjav Poudel <poudelarjav@gmail.com>
2024-03-19 16:49:07 +0000

    add model package

---
+++ cw3_reset/src/main/java/model/Page.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Page {
+    boolean isPrivate;
+    String title;
+    String content;
+
+    public static void Page(String foo, String bar, boolean baz) {
+        System.out.println("output");
+    }
+
+}
+
