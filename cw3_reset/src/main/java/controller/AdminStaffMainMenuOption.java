8635ac33280d671f17cbc1c6a1e3a64587334dbc
CodeVermA <vasuverma1423@gmail.com>
2024-03-19 17:22:43 +0000

    controller

---
+++ cw3_reset/src/main/java/controller/AdminStaffMainMenuOption.java
@@ -0,0 +1,8 @@
+package controller;
+
+public enum AdminStaffMainMenuOption {
+    LOGOUT,
+    MANAGE_QUERIES,
+    ADD_PAGE,
+    MANAGE_FAQ;
+}
---
+++ cw3_reset/src/main/java/controller/Controller.java
@@ -0,0 +1,13 @@
+package controller;
+
+import java.util.Collection;
+
+public class Controller {
+    protected Controller(){
+
+    }
+
+    protected int selectFromMenu(){
+        return 0;
+    }
+}
