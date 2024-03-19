04b8c9f31fe4e0f87032e2f32f3f7ead83aa9698
rossm24 <rossmuir2004@gmail.com>
2024-03-19 17:28:16 +0000

    this might be wrong

---
+++ cw3_reset/src/main/java/view/TextUserInterface.java
@@ -0,0 +1,66 @@
+package view;
+
+public class TextUserInterface implements View {
+
+    @Override
+    public String getInput() {
+        return null;
+    }
+
+    @Override
+    public boolean getYesNoInput() {
+        return true;
+    }
+
+    @Override
+    public void displayInfo() {
+
+    }
+
+    @Override
+    public void displaySuccess() {
+
+    }
+
+    @Override
+    public void displayWarning() {
+
+    }
+
+    @Override
+    public void displayError() {
+
+    }
+
+    @Override
+    public void displayException() {
+
+    }
+
+    @Override
+    public void displayDivider() {
+
+    }
+
+    @Override
+    public void displayFAQ() {
+
+    }
+
+    @Override
+    public void displayFAQSection() {
+
+    }
+
+    @Override
+    public void displayInquiry() {
+
+    }
+
+    @Override
+    public void displaySearchResults() {
+
+    }
+
+
+}
