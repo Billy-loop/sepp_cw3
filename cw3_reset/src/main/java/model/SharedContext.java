1f0d4547abb65863e700c386be8f7388667ae8c2
CodeVermA <vasuverma1423@gmail.com>
2024-03-22 10:12:41 +0000

    shared content + inquiry class

--- cw3_reset/src/main/java/model/SharedContext.java
+++ cw3_reset/src/main/java/model/SharedContext.java
@@ -1,4 +1,11 @@
 package model;
 
+import java.util.Collection;
+import java.util.Map;
+
 public class SharedContext {
+    public static String ADMIN_STAFF_EMAIL;
+    private Map<String, Collection<String>> faqTopicsUpdateSubscribers;
+
+
 }
