59619f8be6a5aa9aae722edc5460ad9f5878a3c7
Billy-loop <148002566+Billy-loop@users.noreply.github.com>
2024-03-24 19:54:30 +0000

    AuthenticatedUserController

--- cw3_reset/src/main/java/controller/AuthenticatedUserController.java
+++ cw3_reset/src/main/java/controller/AuthenticatedUserController.java
@@ -7,7 +7,7 @@ import view.View;
 
 public class AuthenticatedUserController extends Controller{
     public AuthenticatedUserController(SharedContext sharedContext, View view, AuthenticationService authenticationService, EmailService emailService){
-
+        super(sharedContext, view, authenticationService, emailService);
     }
 
     public void logout(){
