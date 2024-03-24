d9a758fff3fb87268713288a03919e4f5de97496
Billy-loop <148002566+Billy-loop@users.noreply.github.com>
2024-03-24 20:37:30 +0000

    FAQSection3.24

--- cw3_reset/src/main/java/model/FAQSection.java
+++ cw3_reset/src/main/java/model/FAQSection.java
@@ -1,4 +1,24 @@
 package model;
 
 public class FAQSection {
+    private String topic;
+    private boolean isPrivate;
+
+    public FAQSection(String topic){
+        this.topic = topic;
+    }
+    public void addSubsection(FAQSection section){
+
+    }
+    public void additem(String topic, String content){
+
+    }
+
+    public FAQSection getParent(){
+        return new FAQSection(this.topic);
+    }
+
+    public String getTopic(){
+        return this.topic;
+    }
 }
