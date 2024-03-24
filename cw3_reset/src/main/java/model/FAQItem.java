c924aedeeeece7ac159e86cdd96b7fcdc2acdccf
Billy-loop <148002566+Billy-loop@users.noreply.github.com>
2024-03-24 20:37:00 +0000

    FAQitem3.24

--- cw3_reset/src/main/java/model/FAQItem.java
+++ cw3_reset/src/main/java/model/FAQItem.java
@@ -1,4 +1,10 @@
 package model;
 
 public class FAQItem {
+    private String question;
+    private String answer;
+    public FAQItem(String question, String answer){
+        this.question = question;
+        this.answer = answer;
+    }
 }
