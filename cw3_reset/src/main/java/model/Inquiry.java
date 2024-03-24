67e03490ca40d148c1ccb6b30dab9fd95e4bba5f
Billy-loop <148002566+Billy-loop@users.noreply.github.com>
2024-03-24 20:40:04 +0000

    PageSearchRes

--- cw3_reset/src/main/java/model/Inquiry.java
+++ cw3_reset/src/main/java/model/Inquiry.java
@@ -1,41 +1,4 @@
 package model;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeParseException;
-import java.util.Date;
-
 public class Inquiry {
-    private LocalDateTime createdAt;
-    private String inquirerEmail;
-    private String subject;
-    private String content;
-    private String assignedTo;
-
-    public Inquiry(String inquirerEmail, String subject, String content){
-        this.createdAt = LocalDateTime.now();
-        this.inquirerEmail = inquirerEmail;
-        this.subject = subject;
-        this.content = content;
-        this.assignedTo = SharedContext.ADMIN_STAFF_EMAIL;
-    }
-
-    public LocalDateTime getCreatedAt(){
-        return createdAt;
-    }
-
-    public String getInquirerEmail() {
-        return inquirerEmail;
-    }
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public String getAssignedTo() {
-        return assignedTo;
-    }
 }
--- cw3_reset/src/main/java/model/PageSearchResult.java
+++ cw3_reset/src/main/java/model/PageSearchResult.java
@@ -1,4 +1,10 @@
 package model;
 
 public class PageSearchResult {
+    private String formattedContent;
+    public PageSearchResult(String formattedContent){
+        this.formattedContent = formattedContent;
+    }
+
+    public String getFormattedContent() {return this.formattedContent;}
 }
