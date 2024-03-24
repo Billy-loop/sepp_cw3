f1e1580e6ffe8eb140f0561337babf6334ba385b
Billy-loop <148002566+Billy-loop@users.noreply.github.com>
2024-03-24 20:36:09 +0000

    AuthenticatedUser

--- cw3_reset/src/main/java/model/AuthenticatedUser.java
+++ cw3_reset/src/main/java/model/AuthenticatedUser.java
@@ -1,4 +1,28 @@
 package model;
 
-public class AuthenticatedUser {
+
+public class AuthenticatedUser extends User {
+    private String email;
+    private String role;
+
+    public AuthenticatedUser(String email, String role){
+        if (email == null){
+            throw new IllegalArgumentException("User email cannot be null");
+
+        }
+        if (!role.equals("TeachingStaff") && !role.equals("Student") && !role.equals("AdminStaff")){
+            throw  new IllegalArgumentException("Unsupported user role");
+        }
+        this.email = email;
+        this.role = role;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public String getRole(){
+        return role;
+    }
+
 }
