d4d3b5841c9577cd1e033e0035c134d51a3e33ff
Billy-loop <148002566+Billy-loop@users.noreply.github.com>
2024-03-24 19:56:58 +0000

    GuestController

--- cw3_reset/src/main/java/controller/GuestController.java
+++ cw3_reset/src/main/java/controller/GuestController.java
@@ -2,17 +2,43 @@ package controller;
 
 import external.AuthenticationService;
 import external.EmailService;
+import model.AuthenticatedUser;
 import model.SharedContext;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import view.View;
+
 
 public class GuestController extends Controller {
     public GuestController(SharedContext sharedContext, View view, AuthenticationService authenticationService, EmailService emailService){
-
+        super(sharedContext, view, authenticationService, emailService);
     }
 
-    public void login(){
+    public void login() {
+        String username = view.getInput("Enter your username: ");
+        String password = view.getInput("Enter your password: ");
+        String response = authenticationService.login(username, password);
+        if (response.contains("error")){
+            view.displayError(response);
+            view.displayInfo("LOGIN Failure");
+            return;
+        }
+        try{
+            JSONParser parser = new JSONParser();
+            JSONObject user = (JSONObject) parser.parse(response);
+            String email = (String) user.get("email");
 
+            String role = (String) user.get("role");
+
+            AuthenticatedUser currnetUser = new AuthenticatedUser(email, role);
+
+            sharedContext.setCurrentUser(currnetUser);
+            view.displaySuccess("successfully logged in");
+
+        } catch (IllegalArgumentException | ParseException e) {
+            view.displayException(e);
+            view.displayInfo("LOGIN Failure");
+        }
     }
-
-
 }
