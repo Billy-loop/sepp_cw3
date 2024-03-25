43c1f5b7696c76085300be2b9e0a49e0933d3d49
Billy-loop <148002566+Billy-loop@users.noreply.github.com>
2024-03-25 17:34:24 +0000

    A work main

--- cw3_reset/src/main/java/Main.java
+++ cw3_reset/src/main/java/Main.java
@@ -1,6 +1,32 @@
+import controller.MenuController;
+import external.AuthenticationService;
+import external.EmailService;
+import external.MockAuthenticationService;
+import external.MockEmailService;
+import model.SharedContext;
+import org.json.simple.parser.ParseException;
+import view.TextUserInterface;
+import view.View;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private SharedContext sharedContext;
+    private AuthenticationService authenticationService;
+    private View view;
+    private EmailService emailService;
+    private MenuController menu;
+
+    public Main() throws URISyntaxException, IOException, ParseException, NullPointerException {
+        sharedContext = new SharedContext();
+        authenticationService = new MockAuthenticationService();
+        emailService = new MockEmailService();
+        view = new TextUserInterface(System.in);
+        menu = new MenuController(sharedContext, view, authenticationService, emailService);
+        menu.mainMenu();
+    }
+    public static void main(String[] args) throws URISyntaxException, IOException, ParseException,NullPointerException {
+        Main main = new Main();
     }
 }
